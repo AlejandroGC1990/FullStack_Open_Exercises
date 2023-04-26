1a529802fd23a9799a63d9b1694afcdc3950e802
AlejandroGarcía Carmona <alejandrogarciacarmona@Mac-mini-de-AlejandroGarcia.local>
2023-04-26 15:59:10 +0200

    1.12

--- Part1/introduction_to_react/Exercises1.1-1.5/src/index.js
+++ Part1/introduction_to_react/Exercises1.1-1.5/src/index.js
@@ -1,107 +1,38 @@
 import React, { useState } from 'react'
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom';
 
-const Button = (props) => (
-  <button onClick={props.handleClick}>{props.text}</button>
-)
+const App = (props) => {
+  const [selected, setSelected] = useState(0)
 
-// const Statistics = (props) => {
-//   if (parseInt(props.value) === 0) {
-//     return (
-//       <div>
-//         <p>{props.text} No feedback given</p>
-//       </div>
-//     )
-//   } else {
-//     return (
-//       <p>{props.text} {props.value}</p>
-//     );
-//   }
-// }
-
-const Statistic = ({ text, value }) => (
-  <tr>
-    <td>{text}</td>
-    <td>{value}</td>
-  </tr>
-);
-
-const Statistics = ({ good, neutral, bad, all, average, positive }) => {
-  if (all === 0) {
-    return (
-      <div>
-        <p>No feedback given</p>
-      </div>
-    )
-  } else {
-    return (
-      <table>
-        <thead>
-          <tr>
-            <th>Statistic</th>
-            <th>Value</th>
-          </tr>
-        </thead>
-        <tbody>
-          <Statistic text="good" value={good} />
-          <Statistic text="neutral" value={neutral} />
-          <Statistic text="bad" value={bad} />
-          <Statistic text="all" value={all} />
-          <Statistic text="average" value={`${average}%`} />
-          <Statistic text="positive" value={`${positive}%`} />
-        </tbody>
-      </table>
-    );
-  }
-}
-
-const App = () => {
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
-  const [all, setAll] = useState(0);
-
-  const formartAverage = (votes) => {
-    return all === 0 ? 0 : ((votes / all) * 100).toFixed(2)
-  }
-
-  const average = formartAverage(bad);
-  const positive = formartAverage(good);
-  
-  const handleGoodVoteClick = () => {
-    setGood(good + 1);
-    setAll(all + 1);
-  }
-  const handleNeutralVoteClick = () => {
-    setNeutral(neutral + 1);
-    setAll(all + 1);
-  }
-  const handleBadVoteClick = () => {
-    setBad(bad + 1);
-    setAll(all + 1);
+  const changeAnec = () => {
+    const numRandom = Math.floor(Math.random() * 100);
+    console.log({numRandom});
+    
+    const selected = numRandom % 6;
+    console.log({selected});
+    setSelected(selected);
   }
 
   return (
     <div>
-      <div>
-        <h1>give feddBack</h1>
-        <Button handleClick={handleGoodVoteClick} text="good" />
-        <Button handleClick={handleNeutralVoteClick} text="neutral" />
-        <Button handleClick={handleBadVoteClick} text="bad" />
-        <h1>statistics</h1>
-        <Statistics good={good} neutral={neutral} bad={bad} all={all} average={average} positive={positive} />
-
-        {/* <Statistics text="good" value={`${good}`} />
-        <Statistics text="neutral" value={`${neutral}`} />
-        <Statistics text="bad" value={`${bad}`} />
-        <Statistics text="all" value={`${all}`} />
-        <Statistics text="average" value={`${average}%`} />
-        <Statistics text="positive" value={`${positive}%`} /> */}
-      </div>
+      <button onClick={changeAnec}>Other anecdote</button>
+      {props.anecdotes[selected]}
     </div>
   )
 }
 
-ReactDOM.render(<App />,
+const anecdotes = [
+  'If it hurts, do it more often',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+];
+
+
+
+ReactDOM.render(
+  <App anecdotes={anecdotes} />,
   document.getElementById('root')
 )
