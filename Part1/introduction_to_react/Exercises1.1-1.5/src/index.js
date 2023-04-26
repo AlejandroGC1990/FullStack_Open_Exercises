ddb9da316cad4294a39b8a095b7dd70b454d1cbb
AlejandroGarcía Carmona <alejandrogarciacarmona@Mac-mini-de-AlejandroGarcia.local>
2023-04-26 13:20:42 +0200

    1.7

--- Part1/introduction_to_react/Exercises1.1-1.5/src/index.js
+++ Part1/introduction_to_react/Exercises1.1-1.5/src/index.js
@@ -7,23 +7,32 @@ const Button = (props) => (
 
 const App = () => {
   // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+  const all = good + neutral + bad;
+  const formartAverage = (votes) => {
+    return all === 0 ? 0 : ((votes / all)*100).toFixed(2)
+  }
+  const average = formartAverage(bad);
+  const positive = formartAverage(good);
 
   return (
     <div>
       <div>
         <h1>give feddBack</h1>
         <Button handleClick={() => setGood(good + 1)} text="good" />
-        <Button handleClick={() => setNeutral(neutral + 1)} text="reset" />
-        <Button handleClick={() => setBad(bad + 1)} text="increment" />
+        <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
+        <Button handleClick={() => setBad(bad + 1)} text="bad" />
       </div>
       <div>
         <h1>statistics</h1>
         <p>good {good}</p>
         <p>neutral {neutral}</p>
         <p>bad {bad}</p>
+        <p>all {all}</p>
+        <p>average {average}%</p>
+        <p>positive {positive}%</p>
       </div>
     </div>
   )
