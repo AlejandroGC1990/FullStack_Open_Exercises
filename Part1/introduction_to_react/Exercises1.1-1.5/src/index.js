a69b8095ea0a9e03eac91bb3ac6fe6b5b3549f96
AlejandroGarcía Carmona <alejandrogarciacarmona@Mac-mini-de-AlejandroGarcia.local>
2023-04-26 15:23:50 +0200

    1.11

--- Part1/introduction_to_react/Exercises1.1-1.5/src/index.js
+++ Part1/introduction_to_react/Exercises1.1-1.5/src/index.js
@@ -5,16 +5,52 @@ const Button = (props) => (
   <button onClick={props.handleClick}>{props.text}</button>
 )
 
-const Statistics = (props) => {
-  if (parseInt(props.value) === 0) {
+// const Statistics = (props) => {
+//   if (parseInt(props.value) === 0) {
+//     return (
+//       <div>
+//         <p>{props.text} No feedback given</p>
+//       </div>
+//     )
+//   } else {
+//     return (
+//       <p>{props.text} {props.value}</p>
+//     );
+//   }
+// }
+
+const Statistic = ({ text, value }) => (
+  <tr>
+    <td>{text}</td>
+    <td>{value}</td>
+  </tr>
+);
+
+const Statistics = ({ good, neutral, bad, all, average, positive }) => {
+  if (all === 0) {
     return (
       <div>
-        <p>{props.text} No feedback given</p>
+        <p>No feedback given</p>
       </div>
     )
   } else {
     return (
-      <p>{props.text} {props.value}</p>
+      <table>
+        <thead>
+          <tr>
+            <th>Statistic</th>
+            <th>Value</th>
+          </tr>
+        </thead>
+        <tbody>
+          <Statistic text="good" value={good} />
+          <Statistic text="neutral" value={neutral} />
+          <Statistic text="bad" value={bad} />
+          <Statistic text="all" value={all} />
+          <Statistic text="average" value={`${average}%`} />
+          <Statistic text="positive" value={`${positive}%`} />
+        </tbody>
+      </table>
     );
   }
 }
@@ -53,12 +89,14 @@ const App = () => {
         <Button handleClick={handleNeutralVoteClick} text="neutral" />
         <Button handleClick={handleBadVoteClick} text="bad" />
         <h1>statistics</h1>
-        <Statistics text="good" value={`${good}`} />
+        <Statistics good={good} neutral={neutral} bad={bad} all={all} average={average} positive={positive} />
+
+        {/* <Statistics text="good" value={`${good}`} />
         <Statistics text="neutral" value={`${neutral}`} />
         <Statistics text="bad" value={`${bad}`} />
         <Statistics text="all" value={`${all}`} />
         <Statistics text="average" value={`${average}%`} />
-        <Statistics text="positive" value={`${positive}%`} />
+        <Statistics text="positive" value={`${positive}%`} /> */}
       </div>
     </div>
   )
