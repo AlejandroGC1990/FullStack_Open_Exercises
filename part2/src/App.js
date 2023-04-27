e3b0e195792a69a06291939c1cda93511a604f1f
AlejandroGarcía Carmona <alejandrogarciacarmona@Mac-mini-de-AlejandroGarcia.local>
2023-04-27 10:54:10 +0200

    2.6

---
+++ part2/src/App.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { id: uuidv4(), name: 'Arto Hellas' }
+  ])
+  const [newName, setNewName] = useState('')
+
+  const handleAddPerson = (event) => {
+    event.preventDefault()
+    const newPerson = { id: uuidv4(), name: newName }
+    setPersons(persons.concat(newPerson))
+    setNewName('')
+  }
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={handleAddPerson}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          <button type="submit" >add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map(persons => <p key={persons.id}>{persons.name}</p>)}
+    </div>
+  )
+}
+
+export default App
