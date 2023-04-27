54a660fdce0db6389849b8d3372a34c092af0d5a
AlejandroGarcía Carmona <alejandrogarciacarmona@Mac-mini-de-AlejandroGarcia.local>
2023-04-27 12:00:42 +0200

    2.10

--- part2/src/App.js
+++ part2/src/App.js
@@ -1,6 +1,60 @@
 import React, { useEffect, useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 
+const Filter = (props) => {
+  return (
+    <div>
+      <p>
+        filter show whit <input value={props.filter} onChange={props.onChange} />
+      </p>
+    </div>
+  );
+}
+
+const PersonForm = (props) => {
+  const handleAddPerson = (event) => {
+    event.preventDefault();
+    const isDuplicateName = props.persons.some(person => person.name === props.newName)
+    const isDuplicateNumber = props.persons.some(person => person.number === props.newNumber)
+    if (isDuplicateName || isDuplicateNumber) {
+      alert(`The name or number is already added to the phonebook`)
+      return
+    }
+    const newPerson = { id: uuidv4(), name: props.newName, number: props.newNumber }
+    props.setPersons(props.persons.concat(newPerson))
+    props.setNewName('')
+    props.setNewNumber('')
+  }
+
+  return (
+    <form onSubmit={handleAddPerson}>
+      <div>
+        name: <input value={props.newName} onChange={props.handleNameChange} />
+      </div>
+      <div>
+        number: <input value={props.newNumber} onChange={props.handleNumberChange} />
+      </div>
+      <div>
+        <button type="submit" >add</button>
+      </div>
+    </form>
+  );
+}
+
+const Persons = (props) => {
+  const [filteredPersons, setFilteredPersons] = useState(props.persons)
+
+  useEffect(() => {
+    const filtered = props.persons.filter(person => person.name.toLowerCase().includes(props.filter.toLowerCase()))
+    setFilteredPersons(filtered)
+  }, [props.persons, props.filter])
+
+  return (
+    <div>
+      {filteredPersons.map(person => <p key={person.id}>{person.name}  {person.number}</p>)}
+    </div>
+  );
+}
 
 const App = () => {
   const [persons, setPersons] = useState([
@@ -9,26 +63,6 @@ const App = () => {
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filter, setFilter] = useState('')
-  const [filteredPersons, setFilteredPersons] = useState(persons)
-
-  useEffect(() => {
-    const filtered = persons.filter(person => person.name.toLowerCase().includes(filter.toLowerCase()))
-    setFilteredPersons(filtered)
-  }, [persons, filter])
-
-  const handleAddPerson = (event) => {
-    event.preventDefault();
-    const isDuplicateName = persons.some(person => person.name === newName)
-    const isDuplicateNumber = persons.some(person => person.number === newNumber)
-    if (isDuplicateName || isDuplicateNumber) {
-      alert(`The name or number is already added to the phonebook`)
-      return
-    }
-    const newPerson = { id: uuidv4(), name: newName, number: newNumber }
-    setPersons(persons.concat(newPerson))
-    setNewName('')
-    setNewNumber('')
-  }
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
@@ -43,27 +77,14 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
-      <div>
-        <p>
-          filter show whit <input value={filter} onChange={handleFilterChange}/>
-        </p>
-      </div>
+      <Filter value={filter} onChange={handleFilterChange} />
       <h2>Add a new</h2>
-      <form onSubmit={handleAddPerson}>
-        <div>
-          name: <input value={newName} onChange={handleNameChange} />
-        </div>
-        <div>
-          number: <input value={newNumber} onChange={handleNumberChange} />
-        </div>
-        <div>
-          <button type="submit" >add</button>
-        </div>
-      </form>
+      <PersonForm persons={persons} newName={newName} newNumber={newNumber} setPersons={setPersons} setNewName={setNewName} setNewNumber={setNewNumber} handleNameChange={handleNameChange} handleNumberChange={handleNumberChange} />
       <h2>Numbers</h2>
-      {filteredPersons.map(person => <p key={person.id}>{person.name}  {person.number}</p>)}
+      <Persons persons={persons} filter={filter} />
     </div>
   )
 }
+
 
 export default App
