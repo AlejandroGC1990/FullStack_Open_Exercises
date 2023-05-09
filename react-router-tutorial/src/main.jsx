2f6ea57b7cad150040a07cd409fbd75049e92718
AlejandroGarcía Carmona <alejandrogarciacarmona@Mac-mini-de-AlejandroGarcia.local>
2023-05-09 17:38:49 +0200

    ruta raiz

--- react-router-tutorial/src/main.jsx
+++ react-router-tutorial/src/main.jsx
@@ -4,12 +4,13 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from 'react-router-dom';
+import Root from './routes/root';
 import './index.css'
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <div> Hello world</div>
+    element: <Root />
   },
 ]);
 
