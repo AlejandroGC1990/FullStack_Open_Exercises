15ac9ef72253e4d351aeb22e2c4da3536e9e4df5
AlejandroGarcía Carmona <alejandrogarciacarmona@Mac-mini-de-AlejandroGarcia.local>
2023-05-09 18:03:02 +0200

    la interfaz de usuario de la ruta de contacto

--- react-router-tutorial/src/main.jsx
+++ react-router-tutorial/src/main.jsx
@@ -6,6 +6,8 @@ import {
 } from 'react-router-dom';
 import Root from './routes/root';
 import ErrorPage from './error-page';
+import Contact from "./routes/contact";
+
 import './index.css'
 
 const router = createBrowserRouter([
@@ -13,6 +15,10 @@ const router = createBrowserRouter([
     path: '/',
     element: <Root />,
     errorElement: <ErrorPage />
+  },
+  {
+    path: "contacts/:contactId",
+    element: <Contact />,
   },
 ]);
 
