830304e63bd7d118bb440bb4aa0ba18000ffc7f1
AlejandroGarcía Carmona <alejandrogarciacarmona@Mac-mini-de-AlejandroGarcia.local>
2023-05-10 12:59:39 +0200

    eliminacion de registros

--- react-router-tutorial/src/main.jsx
+++ react-router-tutorial/src/main.jsx
@@ -13,6 +13,7 @@ import Contact, { loader as contactLoader } from "./routes/contact";
 import EditContact, {
   action as editAction,
 } from './routes/edit';
+import {action as destroyAction} from "./routes/destroy";
 import './index.css'
 
 const router = createBrowserRouter([
@@ -39,6 +40,10 @@ const router = createBrowserRouter([
         element: <EditContact />,
         loader: contactLoader,
         action: editAction,
+      },
+      {
+        path: "contacts/:contactId/destroy",
+        action: destroyAction,
       },
     ],
   },
