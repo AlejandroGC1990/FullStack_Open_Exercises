e85677652e2f97d85a732e4fa9bc6453ed7ceaa3
AlejandroGarcía Carmona <alejandrogarciacarmona@Mac-mini-de-AlejandroGarcia.local>
2023-05-10 13:02:32 +0200

    errores contextuales

--- react-router-tutorial/src/main.jsx
+++ react-router-tutorial/src/main.jsx
@@ -44,6 +44,7 @@ const router = createBrowserRouter([
       {
         path: "contacts/:contactId/destroy",
         action: destroyAction,
+        errorElement: <div>Oops! There was an error.</div>,
       },
     ],
   },
