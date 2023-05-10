7f4aa67ec94c9f275636e4c6d2585a9f8558aedb
AlejandroGarcía Carmona <alejandrogarciacarmona@Mac-mini-de-AlejandroGarcia.local>
2023-05-10 11:11:39 +0200

    rutas anidadas

--- react-router-tutorial/src/main.jsx
+++ react-router-tutorial/src/main.jsx
@@ -14,11 +14,17 @@ const router = createBrowserRouter([
   {
     path: '/',
     element: <Root />,
-    errorElement: <ErrorPage />
-  },
-  {
-    path: "contacts/:contactId",
-    element: <Contact />,
+    errorElement: <ErrorPage />,
+    children: [
+      {//Queremos que el componente de contacto se muestre dentro del <Root>diseño. 
+        //Lo hacemos haciendo que la ruta de contacto sea secundaria de la ruta raíz.
+        //Ahora volverá a ver el diseño raíz, pero una página en blanco a la derecha. 
+        //Necesitamos decirle a la ruta raíz dónde queremos que represente sus rutas secundarias. 
+        //Eso lo hacemos con <Outlet> en root.jsx..
+        path: "contacts/:contactId",
+        element: <Contact />,
+      },
+    ],
   },
 ]);
 
