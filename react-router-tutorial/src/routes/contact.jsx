d559390bfba1cf15a842fdf6285f72a79185751e
AlejandroGarcía Carmona <alejandrogarciacarmona@Mac-mini-de-AlejandroGarcia.local>
2023-05-10 15:31:46 +0200

    datos no encontrados

--- react-router-tutorial/src/routes/contact.jsx
+++ react-router-tutorial/src/routes/contact.jsx
@@ -14,6 +14,12 @@ export async function action({request, params}){
 
 export async function loader({ params }) {
     const contact = await getContact(params.contactId);
+    if(!contact){
+        throw new Response("", {
+            status: 404,
+            statusText: "Not Found",
+        });
+    }
     return { contact };
 }
 
